dea417d1065bca4daf1a9b207b91eb617aa0488d
Julia Zuara <julia_any@hotmail.com>
2015-04-29 10:58:40 +0200

    Añadida la clase WorldOfZuul y el metodo maim

---
+++ WorldOfZuul.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class WorldOfZuul here.
+ * 
+ * @author Julia Zuara
+ * @version (a version number or a date)
+ */
+public class WorldOfZuul
+{
+
+   public static void main(String [] args){
+       Game game = new Game();
+       game.play();
+    }
+}
